0b5d858743767024db350789f12cad6e7a350b7f
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2022-12-19 18:22:59 +0100

    add export

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -37,7 +37,7 @@ function GameScreen() {
 }
 
 // function returns a random color object with r,g and b values between 0 and 255
-function getRandomColor() {
+export function getRandomColor() {
   // Generate random color gradients between 0 and max 256 (excluding 256)
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
