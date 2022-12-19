8f591785d153ea7fe5820398676051d15066c1b5
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2022-12-19 18:09:13 +0100

    create GameScreen2 with 2. game mode

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import GameScreen from './screens/GameScreen';
+import GameScreen2 from './screens/GameScreen2';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <GameScreen />
+      <GameScreen2 />
       <StatusBar style="auto" />
     </View>
   );
