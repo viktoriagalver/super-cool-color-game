8b0676ec86ba67af7c36deb9bab106b701b2bc44
D2/CVS-x3 - Raab, Tobias <64197026+tobiasraab@users.noreply.github.com>
2022-12-19 18:07:45 +0100

    generate disguise colors for basic game

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,11 +5,16 @@ function GameScreen() {
   // get a random color to look for
   const randomColor = getRandomColor()
 
-  //TODO: logic for color creation
+  // get 3 random colors. differenceSum defines how equal/unequal they look.
+  const differenceSum = 50
+  const disguiseColors = getAllDisguiseColors(randomColor, differenceSum)
+
+  // array with all 4 colors
+  let colorsInField = disguiseColors
+  colorsInField.push(randomColor)
 
   return (
     <View style={styles.gameScreenContainer}>
-
       {/* Task and RGB color */}
       <View style={styles.taskField}>
         <Text style={styles.explanationText}>
@@ -22,17 +27,36 @@ function GameScreen() {
 
       {/* Color Selection Field */}
       <View style={styles.colorField}>
-        <ColorFieldButton color="#D05353" />
-        <ColorFieldButton color="#E58F65" />
-        <ColorFieldButton color="#F9E784" />
-        <ColorFieldButton color="#F1E8B8" />
+        <ColorFieldButton color={colorObjectToString(colorsInField[0])} />
+        <ColorFieldButton color={colorObjectToString(colorsInField[1])} />
+        <ColorFieldButton color={colorObjectToString(colorsInField[2])} />
+        <ColorFieldButton color={colorObjectToString(colorsInField[3])} />
       </View>
-
     </View>
   );
 }
 
-// function returns a random color object with r,g and b values between 0 and 255
+// shuffles an array
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    // Generate random number
+    var j = Math.floor(Math.random() * (i + 1));
+
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
+
+// converts color object to rgb() string
+function colorObjectToString(color) {
+  const colorString = "rgb(" + color.r + "," + color.g + "," + color.b + ")"
+  return colorString
+}
+
+// Generate a random color object with r,g and b values between 0 and 255
 function getRandomColor() {
   // Generate random color gradients between 0 and max 256 (excluding 256)
   const red = Math.floor(Math.random() * 256);
@@ -44,6 +68,94 @@ function getRandomColor() {
     g: green,
     b: blue
   }
+}
+
+// Get random numbers that add up to a given sum
+function getRandomPointsToSum(sum) {
+  // generate random points to divide a scale
+  let randomPoints = [];
+  randomPoints.push(Math.floor(Math.random() * (sum + 1)));
+  randomPoints.push(Math.floor(Math.random() * (sum + 1)));
+  // sort points from low to high
+  randomPoints.sort((a, b) => {
+    return a - b;
+  });
+
+  // calc length of scale
+  const randomInt_1 = randomPoints[0];
+  const randomInt_2 = randomPoints[1] - randomPoints[0];
+  const randomInt_3 = sum - randomPoints[1];
+
+  // return an array of 3 random points which add up to sum
+  return [randomInt_1, randomInt_2, randomInt_3];
+}
+
+// get a disguise color
+function calcDisguiseColor (color, colorDiff) {
+  let color_result = {
+    r: undefined,
+    g: undefined,
+    b: undefined
+  };
+
+  // r value
+  if (color.r - colorDiff[0] >= 0 && color.r + colorDiff[0] <= 255) {
+    const direction = Math.round(Math.random());
+    if (direction < 1) {
+      color_result.r = color.r - colorDiff[0];
+    } else {
+      color_result.r = color.r + colorDiff[0];
+    }
+  } else if (color.r - colorDiff[0] >= 0) {
+    color_result.r = color.r - colorDiff[0];
+  } else {
+    color_result.r = color.r + colorDiff[0];
+  }
+
+  // g value
+  if (color.g - colorDiff[1] >= 0 && color.g + colorDiff[1] <= 255) {
+    const direction = Math.round(Math.random());
+    if (direction < 1) {
+      color_result.g = color.g - colorDiff[1];
+    } else {
+      color_result.g = color.g + colorDiff[1];
+    }
+  } else if (color.g - colorDiff[1] >= 0) {
+    color_result.g = color.g - colorDiff[1];
+  } else {
+    color_result.g = color.g + colorDiff[1];
+  }
+
+  // b value
+  if (color.b - colorDiff[2] >= 0 && color.b + colorDiff[2] <= 255) {
+    const direction = Math.round(Math.random());
+    if (direction < 1) {
+      color_result.b = color.b - colorDiff[2];
+    } else {
+      color_result.b = color.b + colorDiff[2];
+    }
+  } else if (color.b - colorDiff[2] >= 0) {
+    color_result.b = color.b - colorDiff[2];
+  } else {
+    color_result.b = color.b + colorDiff[2];
+  }
+
+  return color_result
+}
+
+// return an array of 3 disguise colors
+function getAllDisguiseColors(color, differenceSum) {
+  // get 3 color differences
+  const colorDiff_1 = getRandomPointsToSum(differenceSum);
+  const colorDiff_2 = getRandomPointsToSum(differenceSum + 10);
+  const colorDiff_3 = getRandomPointsToSum(differenceSum + 20);
+
+  // return 3 different colors
+  return [
+    calcDisguiseColor(color, colorDiff_1),
+    calcDisguiseColor(color, colorDiff_2),
+    calcDisguiseColor(color, colorDiff_3)
+  ];
 }
 
 
