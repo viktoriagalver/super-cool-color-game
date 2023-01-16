de651be2eca5868e562a08eed1a5a74dce6d61a3
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2023-01-16 19:42:49 +0100

    button color text depending on background color

--- components/Button.js
+++ components/Button.js
@@ -2,17 +2,21 @@ import { StyleSheet, Text, View } from "react-native";
 
 function Button() {
   return(
-    <View style={styles.buttonContainer}>
-      <Text></Text>
-    </View>
+    <Pressable onPress={props.onPress}>
+      <View style={{
+    backgroundColor: props.color,
+    marginBottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+    paddingTop: 10,
+    width: 100,}}>
+        <Text style={{
+    color: props.textColor,
+    fontSize: '20px',}} >{props.text}</Text>
+      </View>
+    </Pressable>
   );
 };
-
-const styles = StyleSheet.create({
-  buttonContainer: {
-    backgroundColor: '#000',
-    color: '#fff',
-  },
-});
 
 export default Button;
--- screens/GameScreen2.js
+++ screens/GameScreen2.js
@@ -8,7 +8,38 @@ function GameScreen2() {
   // get a random color to look for
   const randomColor = getRandomColor();
 
-  const randomRGB = `rgb(${randomColor.r},${randomColor.g},${randomColor.b})`;
+  //get rgb color as string
+  const randomRGB = `rgb(${props.color.r},${props.color.g},${props.color.b})`;
+
+  //button text color depending in background color
+  function getTextColor() {
+    if ((props.color.r + props.color.g + props.color.b) > 350) {
+      return ('#000')
+    } else {
+      return ('#fff')
+    }
+  };
+  const textColor = getTextColor();
+
+  console.log(textColor)
+
+  const [input1, setInput1] = React.useState('');
+  const [input2, setInput2] = React.useState('');
+  const [input3, setInput3] = React.useState('');
+
+  // get User Input as rgb String
+  const userInput = `rgb(${input1},${input2},${input3})`;
+
+  // check if user input is right or wrong 
+  const handlePress = () => {
+    if(userInput === randomRGB) {
+      console.log("WIN!!!")
+    } else {
+      console.log("LOOSE!!!")
+    }
+  }
+
+  console.log(randomRGB);
 
   return( 
   <View style={styles.gameScreenContainer}>
@@ -20,7 +51,8 @@ function GameScreen2() {
         </Text>
         <ColorFieldButton color={randomRGB} />
       </View>
-      <ColorInputField />
+      <ColorInputField input1={input1} input2={input2} input3={input3} onChange1={setInput1} onChange2={setInput2} onChange3={setInput3}/>
+      <Button text='Check' onPress={handlePress} color={randomRGB} textColor={textColor}/>
   </View>
   );
 };
