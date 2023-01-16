cee0ae41cab1222194bb42a8f553720e3fc897fd
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2023-01-16 16:05:31 +0100

    get rgb user input and check

--- screens/GameScreen2.js
+++ screens/GameScreen2.js
@@ -1,14 +1,33 @@
-import { Text, View } from "react-native";
-import { getRandomColor, styles } from "./GameScreen";
+import { StyleSheet, Text, View } from "react-native";
+import { getRandomColor } from "./GameScreen";
 import ColorFieldButton from "../components/ColorFieldButton";
 import ColorInputField from "../components/ColorInputField";
 import Button from "../components/Button";
+import React from "react";
 
 function GameScreen2() {
-  // get a random color to look for
+  // get a random color
   const randomColor = getRandomColor();
-
   const randomRGB = `rgb(${randomColor.r},${randomColor.g},${randomColor.b})`;
+
+  const [input1, setInput1] = React.useState('');
+  const [input2, setInput2] = React.useState('');
+  const [input3, setInput3] = React.useState('');
+
+  // get User Input as RGB String
+  const userInput = `rgb(${input1},${input2},${input3})`;
+
+  // check whether user input is right or wrong 
+  const handlePress = () => {
+    console.log(userInput);
+    if(userInput === randomRGB) {
+      console.log("WIN!!!")
+    } else {
+      console.log("LOOSE!!!")
+    }
+  }
+
+  console.log(randomRGB);
 
   return( 
   <View style={styles.gameScreenContainer}>
@@ -20,9 +39,30 @@ function GameScreen2() {
         </Text>
         <ColorFieldButton color={randomRGB} />
       </View>
-      <ColorInputField />
+      <ColorInputField input1={input1} input2={input2} input3={input3} onChange1={setInput1} onChange2={setInput2} onChange3={setInput3}/>
+      <Button text='Check' onPress={handlePress}/>
   </View>
   );
 };
+
+export const styles = StyleSheet.create({
+  gameScreenContainer: {
+    height: "100%",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    paddingBottom: 100,
+    alignItems: 'center',
+  },
+  explanationText: {
+    fontSize: 16,
+    margin: 12
+  },
+  taskField: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default GameScreen2;
