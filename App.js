2175fc4e05f433a80e6a79d01b832ef6667cc513
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2023-01-17 09:41:09 +0100

    fix errors

--- App.js
+++ App.js
@@ -126,7 +126,7 @@ export default function App() {
       );
       break;
     case "GameScreen2":
-      currentScreenJSX = <GameScreen2 />;
+      currentScreenJSX = <GameScreen2 color={randomColor}/>;
       break;
   }
 
